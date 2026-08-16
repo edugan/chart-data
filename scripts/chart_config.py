5de83deb723ae7f52a4f5fe6c1404148a602704e
edugan <elidugan@hotmail.com>
2026-08-16 13:36:19 -0400

    added bubbling-under

--- scripts/chart_config.py
+++ scripts/chart_config.py
@@ -1,9 +1,11 @@
 CHARTS = {
     "hot-100": {"slug": "hot-100", "start_date": "1958-08-04", "genre": "All"},
     "billboard-200": {"slug": "billboard-200", "start_date": "1963-08-17", "genre": "All"},
+    "bubbling-under": {"slug": "bubbling-under-hot-100-singles", "start_date": "1992-12-05", "genre": "All"},
     "pop-radio": {"slug": "pop-songs", "start_date": "1992-10-03", "genre": "Pop"},
     "country-radio": {"slug": "country-airplay", "start_date": "1990-01-20", "genre": "Country"},
     "alt-radio": {"slug": "alternative-airplay", "start_date": "1988-09-10", "genre": "Rock"},
+    # "NAME": {"slug": "URL", "start_date": "DATE", "genre": "GENRE"},
 }
 
 # Fixed, canonical list -- deliberately includes genres with no charts yet
